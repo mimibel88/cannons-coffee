c528152680b5c603f03898ff47e7c4cfaa936dc4
mimibel88 <mbelmodis88@gmail.com>
2026-08-13 13:01:23 -0700

    first commit

---
+++ JS/custom-inquiry.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("#form")
+
+function onFormSubmit(event) {
+    event.preventDefault()
+
+    const data = new FormData(event.target)
+    const dataObject = Object.fromEntries(data.entries())
+
+    console.log(dataObject)
+
+    const interest = dataObject.buyOrWork === "buy" ? "buying coffee" : "seeking employment"
+
+    const message = `Thank you ${dataObject.yourname}! We appreciate your interest in ${interest}. Your favorite coffee flavor is ${dataObject.flavor} and your comments are ${dataObject.comments}. We will soon contact you at ${dataObject.email} or ${dataObject["phone-number"]}.`
+
+    const thankYouMessage = document.querySelector("#thank-you")
+    thankYouMessage.textContent = message
+    thankYouMessage.classList.remove("hidden")
+
+    form.reset()
+}
+
+form.addEventListener("submit", onFormSubmit)
